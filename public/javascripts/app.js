1e8d4f6c2634a8958fba7cfc706a787af3ba528f
saumya <2saumyaray@gmail.com>
2017-11-11 19:36:50 +0530

    trying to play the clicked video

---
+++ public/javascripts/app.js
@@ -0,0 +1,17 @@
+//
+// version : 0.1.0
+
+(function(){
+	console.log('app.js');
+	var onButtonClick = function(eventObj){
+		console.log('onButtonClick');
+	}
+})()
+
+
+// ==============   Globals =========================
+var onButtonClick = function(eventObj){
+	console.log('onButtonClick',eventObj);
+}
+// ============== / Globals =========================
+
