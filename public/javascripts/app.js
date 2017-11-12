d768c3e999dd211c3dec9971affad532c8c1e0f7
saumya <2saumyaray@gmail.com>
2017-11-12 13:00:03 +0530

    playing of video is done for mp4 files only

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,6 +12,17 @@
 // ==============   Globals =========================
 var onButtonClick = function(eventObj){
 	console.log('onButtonClick',eventObj);
+
+	console.log(eventObj.length);
+	//removing white space
+	var s1 = eventObj.replace(" ","");
+	// setting info
+	var videoInfo = document.getElementById("idVideoInfo");
+	videoInfo.innerHTML='Playing :'+s1;
+	// setting video
+	var videoPath = '../zz_z/'+s1;
+	var vElm = document.getElementById("idVideo");
+	vElm.src = videoPath;
 }
 // ============== / Globals =========================
 
