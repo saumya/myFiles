d0388eb2568b53a683d94de0ea07d334c5d42a97
saumya <2saumyaray@gmail.com>
2017-11-15 17:15:27 +0530

    wip, files in the folder

--- routes/files.js
+++ routes/files.js
@@ -1,9 +1,29 @@
+//
 var express = require('express');
 var router = express.Router();
+//
+var fs = require('fs');
+var path = require("path");
+//
+const folderPath = path.join('./public/zz_z/');
 
-/* GET users listing. */
+/* Get the Files */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('response');
+  res.render('files', { title: 'My File details in a folder.' });
 });
+
+router.get('/folder1', function(req, res, next) {
+  res.send('response/ folder1');
+});
+
+router.get('/folder2', function(req, res, next) {
+  res.send('response/ folder2');
+});
+
+router.get('/inFolder', function(request, response, next){
+	response.send('Files in Folder');
+})
+
 
 module.exports = router;
