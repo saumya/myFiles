10271aa8b746f640094e661d4eff6a5faa5c93aa
saumya <2saumyaray@gmail.com>
2017-11-15 16:45:02 +0530

    Started working on files

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var me = require('./routes/me');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const me = require('./routes/me');
+const files = require('./routes/files');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/me', me);
+app.use('/files', files);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
