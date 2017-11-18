f59eec3d236000835b6af93b9b247ab337d84118
saumya <2saumyaray@gmail.com>
2017-11-19 00:44:02 +0530

    done with refresh button

--- public/javascripts/fileListOne.js
+++ public/javascripts/fileListOne.js
@@ -3,47 +3,35 @@
 
 (function(){
 	console.log('fileListOne.js');
+
 	var onButtonClick = function(eventObj){
-		console.log('onButtonClick');
+		//console.log('onButtonClick');
+		location.reload();
 	}
+	/*
 	//
-	//console.log('aFileDetails[0].aTime',aFileDetails[0].aTime,aFileDetails[0].name);
-	//var sorted = [];
 	var sorted = aFileDetails.sort(function(a,b){
 	  var t1 = new Date(a.aTime).getTime();
 	  var t2 = new Date(b.aTime).getTime();
-
-	  //console.log('t1',t1,':: t2',t2);
-
 	  if(t1>t2){
-	  	//console.log('t1>t2');
 	    return true;
 	  }
-
 	});
-	//console.log('aFileDetails[0].aTime',sorted[0].aTime,sorted[0].name);
-	//console.log('aFileDetails[0].aTime',aFileDetails[0].aTime,aFileDetails[0].name);
-	//
-	/*
-	for (var i = 0; i < sorted.length; i++) {
-		var fDetails = sorted[i];
-		console.log('fDetails.name',fDetails.name);
-	}
-	*/
 
 	var container = document.getElementById("idFileListContainer");
 	container.innerHTML = "";
 
 	var newString = '<ul>';
 	for (var i = sorted.length - 1; i >= 0; i--) {
-		//var fDetails = sorted[i];
-		//console.log('sorted[i]',sorted[i].name);
 		newString += '<li><span class="fName">'+sorted[i].name+'</span><span class="fDate">'+sorted[i].aTime+'</span></li>';
 	}
 	newString += '</ul>';
 
 	container.innerHTML = newString;
+	*/
 
+	var btn = document.getElementById('btnRefresh');
+	btn.addEventListener("click", onButtonClick);
 
 
 	//
@@ -51,6 +39,7 @@
 
 
 // ==============   Globals =========================
+/*
 var onButtonClick = function(eventObj){
 	console.log('onButtonClick',eventObj);
 
@@ -65,5 +54,6 @@ var onButtonClick = function(eventObj){
 	var vElm = document.getElementById("idVideo");
 	vElm.src = videoPath;
 }
+*/
 // ============== / Globals =========================
 
