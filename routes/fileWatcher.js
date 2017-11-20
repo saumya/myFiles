c1a8b72ecf9887f6392746d8e5c9712ca84d0bb0
saumya <2saumyaray@gmail.com>
2017-11-20 16:03:27 +0530

    wip: checking for folder options

--- routes/fileWatcher.js
+++ routes/fileWatcher.js
@@ -1,10 +1,60 @@
+//
 var express = require('express');
 var router = express.Router();
+//
+var chokidar = require('chokidar');
+//var path = require("path");
+
+
+//
+// Initialize watcher.
+/*
+//var watcher = chokidar.watch('file, dir, glob, or array', {
+var watcher = chokidar.watch(['.'], {
+//var watcher = chokidar.watch('./', {
+  ignored: /(^|[\/\\])\../,
+  persistent: true,
+  usePolling: true,
+  interval: 2000,
+  binaryInterval: 2500
+}); 
+*/
+// it wawtches from the 'root' folder
+var watcher = chokidar.watch(['./routes/','./public/zz_z/'], {
+	persistent: true, 
+});
+// Something to use when events are received.
+//var log = console.log.bind(console);
+//
+//watcher.on('change', function(path){ console.log(`File ${path} has been changed`) })
+//watcher.on('error',function(error){ console.log(`Watcher error: ${error}`) });
+//
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log("=== Route / ===");
   //res.render('index', { title: 'My Files.' });
-  res.send("Let's Watch Folder.");
-});
+  //res.send("Let's Watch Folder.");
+  watcher.on('ready', function(){
+  	console.log('==========================');
+  	console.log('Chokidar : Ready');
+  	console.log('==========================');
+  	res.send('Chokidar is Ready.');
+  }) 
+  watcher.on('change', function(path){ 
+  	console.log('==========================');
+  	console.log('Chokidar : Change');
+  	console.log('path',path);
+  	console.log('==========================');
+  	//res.send('change');
+  })
+	watcher.on('error',function(error){
+  	console.log('xxxxxxxxxxxxxxxxxxxxxxxxxx');
+  	console.log('Chokidar : ERROR');
+  	console.log('xxxxxxxxxxxxxxxxxxxxxxxxxx'); 
+		//res.send('Error');
+	});
+	//res.send('Watching folder now.');
+});// END route
 
 module.exports = router;
